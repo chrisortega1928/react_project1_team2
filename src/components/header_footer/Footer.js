05452d9e9fac6f170236068db0c6f4334bebcdf2
mc5rgspider <mc5rgspider@gmail.com>
2022-01-19 11:26:34 -0500

    style change team members

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -10,9 +10,7 @@ import mountain_img from '../../resources/images/img_mountain.png';
 const Footer = () => {
     return (
         /* style edited */
-        <footer className = "bck_grey" style={{background:`url(${mountain_img})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'}}>
+        <footer className = "bck_grey">
             <Fade delay={500}>
                 <div className="font_righteous footer_logo_venue">
                     <Tooltip title="Check out our Github!">
@@ -32,7 +30,9 @@ const Footer = () => {
                 <div className="footer_copyright">JJAY TTP, Team 2. All rights reserved ©</div>
                 <div className="footer_copyright">
 
-                       <div className="team-container">
+                       <div className="team-container" style={{background:`url(${mountain_img})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: '100% 100%'}}>
                        <div className="team-window">
                             <h4 className="team-title">Team leaders</h4>
                             <div className="members">
@@ -43,19 +43,30 @@ const Footer = () => {
                         </div>
                         <div className="team-window">
                             <h4 className="team-title">Team Members</h4>
-                            <div className="members">
-                                <p>Robert</p>
-                                <p>Christopher</p>
-                                <p>Ankit</p>
-                                <p>Kirana</p>
-                                <p>Humayun</p>
-                                <p>Jeryel</p>
-                                <p>Chelsea</p>
-                                <p>Gabriel</p>
-                                <p>Nanami</p>
-                                <p>Michael</p>
-                                <p>Md</p>
-                                <p>Cenk</p>
+                            <div className="members" style={{display:'flex', justifyContent:'center'}}>
+                            
+                                <div class="item">Robert<br></br>
+                                Christopher<br></br>
+                                Ankit<br></br>
+                                </div>
+                                
+                                <div class="item">
+                                Kirana<br></br>
+                                Humayun<br></br>
+                                Jeryel<br></br>
+                                </div>
+                                <div class="item">
+                                Chelsea<br></br>
+                                Gabriel<br></br>
+                                Nanami<br></br>
+                                </div>
+                                <div class="item">
+                                Michaelv<br></br>
+                                Md<br></br>
+                                Cenk<br></br>
+                                </div>
+
+                                
                             </div>
                         </div>
                        </div>
